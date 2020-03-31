ac525eaace16f3b9c151145cf612e1629a5c6367
lizz <374494848@qq.com>
2020-03-31 13:10:15 +0800

    初始化 注册 db 块

--- SiemensDBSimulator/Program.cs
+++ SiemensDBSimulator/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Snap7;
 using System;
+using System.Configuration;
 using System.Text.RegularExpressions;
 
 namespace SiemensDBSimulator
@@ -18,25 +19,52 @@ namespace SiemensDBSimulator
 
             System.Console.WriteLine("Started:" + Error);
 
+            var dbs = ConfigurationManager.AppSettings["dbs"];
+            if (!string.IsNullOrEmpty(dbs))
+            {
+                foreach (var cmd in dbs.Split(new char[] { ';' }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    Reg(cmd);
+                }
+            }
+
             while (true)
             {
                 var cmd = System.Console.ReadLine();
 
-                if (!Regex.IsMatch(cmd, "^[0-9]*,[0-9]*$"))
-                {
-                    System.Console.WriteLine("输入格式不正确,示例[1,1024]");
-                    continue;
-                }
-
-                var numStr = cmd.Split(',')[0];
-                var sizeStr = cmd.Split(',')[1];
-
-                var buf = new byte[Convert.ToInt32(sizeStr)];
-
-                Server.RegisterArea(S7Server.srvAreaDB, Convert.ToInt32(numStr), buf, buf.Length);
-
+                Reg(cmd);
             }
 
+        }
+
+        static bool Reg(string cmd)
+        {
+            string numStr = "0", sizeStr = "0";
+
+            if (Regex.IsMatch(cmd, "^[1-9]{1}[0-9]*,[0-9]*$"))
+            {
+                numStr = cmd.Split(',')[0];
+                sizeStr = cmd.Split(',')[1];
+
+            }
+            else if (Regex.IsMatch(cmd, "^[1-9]{1}[0-9]*$"))
+            {
+                numStr = cmd;
+                sizeStr = "1024";
+            }
+            else
+            {
+                System.Console.WriteLine("输入格式不正确,示例[1,1024]:" + cmd);
+                return false;
+            }
+
+            var buf = new byte[Convert.ToInt32(sizeStr)];
+
+            Server.RegisterArea(S7Server.srvAreaDB, Convert.ToInt32(numStr), buf, buf.Length);
+
+            Console.WriteLine(string.Format("注册成功:DB{0},大小:{1}字节", numStr, sizeStr));
+
+            return true;
         }
 
         static void EventCallback(IntPtr usrPtr, ref S7Server.USrvEvent Event, int Size)
